b1efe7ff6c8c25075fd6df7d929594f83a9a88ce
dvgba <diegovianagomes01@gmail.com>
2025-02-10 19:40:34 -0300

    BOOK-00 "add Springboot folder"

---
+++ src/test/java/com/diegoviana/bookworm_backend/BookwormBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoviana.bookworm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwormBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
